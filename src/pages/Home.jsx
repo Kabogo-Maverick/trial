70b64f0636afd5dcd3ac88f140be6b949d3a510b
mkay <mkay.com>
2025-10-20 20:36:35 +0300

    initial setting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Hero from '../components/Hero';
+import Hero from '../components/home/Hero';
 import About from '../components/home/About';
 import Features from '../components/home/Features';
 import Contact from '../components/home/Contact';
